d9062abb25d3054802529468ad2e0a73dfb812cb
Jing Lin <Jinglin@mit.edu>
2015-09-19 19:02:12 -0400

    Parser complete

--- app/JS/textParser.js
+++ app/JS/textParser.js
@@ -34,38 +34,45 @@ angular.module('myApp', []).controller('textParser', function($scope) {
 					break;
 				}
 			}
-			count = count + 1
+			count = count + 1;
 		}
 
 		for(var i = count; i < $scope.inputText.length; i++) {
-			currLine = $scope.inputText.length[i];
-			indexSym = /@/.exec(currLine)
-			if (indexSym != -1) {
+			var currLine = $scope.inputText.length[i];
+			var indexSymAt = /@/.exec(currLine).index, indexSymPound = /-/.exec(currLine).index;
+			if (indexSymAt != -1) {
 				var nodeData = {
 					name: undefined,
-					parent: undefined,
+					parent: {
+						name: undefined,
+						id: undefined,
+						tier: undefined
+					},
 					text: []
 				};
 				var atRegExp = /[^\\]\(/, matchAt = atRegExp.exec(currLine);
 				if (matchAt.index == -1) {
-					$scope.errorWarning = "Specify a parent description with () on line " + count;
+					$scope.errorWarning = "Specify a parent description with (name) on line " + count;
+					return;
 				}
 				else {
 					firstWordRegExp = /\w/;
 					matchFirstWord = firstWordRegExp.exec(currLine);
 					nodeData.name = currLine.substr(matchFirstWord.index,matchAt.index);
-					var parentNode = {
-						name: undefined,
-						id: undefined
-					};
 					splicedList = currLine.substr(matchAt.index + matchAt[0].length);
 					var idRegExp = /id\s*=/, matchId = idRegExp.exec(splicedList);
 					if (matchAt.index == -1) {
 						parentNode.id = undefined;
 						parenthesisRegExp = /\([\s\w]*\)/;
 						var name = parenthesisRegExp.exec(splicedList);
-						if (name[0].length != 0)
+						if (name[0].length != 0) {
 							parentNode.name = name[0];
+						}
+						else {
+							$scope.errorWarning = "Node requires a parent name on line " + count;
+							return;
+						}
+
 					}
 					else {
 						var parentName = splicedList.substr(0, idRegExp.index), spaceBeforeRegExp = /\s\w/;
@@ -75,10 +82,28 @@ angular.module('myApp', []).controller('textParser', function($scope) {
 							parentName = parentName.substr(matchSpaceBefore.index);
 
 						var commaSpaceRegExp = /[\s]*,[\s]*/;
-						
+						var matchCommaSpace = commaSpaceRegExp.exec(parentName);
+						if (matchCommaSpace.index == -1) {
+							$scope.errorWarning = "Separate parent name and id with a comma on line " + count;
+							return;
+						}
+						else {
+							var poundSpaceRegExp = /#/, matchPound = poundSpaceRegExp.exec(parentName);
+							if (matchPound.index == -1) {
+								$scope.errorWarning = "Parent name requires an # symbol on line " + count;
+								return;
+							}
+							else {
+								node.parent.name = parentName.substr(matchPound.index + 1, commaSpaceRegExp.index);
+							}
+						}
 
 					}
 				}
+				nodeList.append(nodeData)
+			}
+			else if (indexSymPound != -1) {
+				nodeList[-1].text.append(currLine.substr(indexSymPound + 1));
 			}
 		}
 	};

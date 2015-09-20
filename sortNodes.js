92fe5831f47aebfe1ff16dd711d944c40d88c117
Samuel <samjsong@mit.edu>
2015-09-19 22:13:56 -0400

    sortNodes fixed

--- sortNodes.js
+++ sortNodes.js
@@ -4,38 +4,35 @@ function sortNodes(nodeList) {
 	var sortedNodes = [];
 
 	while (nodeList.length > 0) {
-		for (i=0; i < nodeList.length; i++) {
+		for (var i=0; i < nodeList.length; i++) {
 			node = nodeList[i];
 
 			// Find & sort the head node (tier = 0)
 			if (node.parent.name == undefined) {
-				node.tier = 0;
 				nodeList.splice(i,1);
 				sortedNodes[0] = [node];
 			}
 
 			// All other nodes (tier > 0)
 			else {
-				// If the parent hasn't been assigned a tier yet, skip it;
-				// we'll come back to it through the while loop.
-				if node.parent.tier == undefined {
-					continue;
-				}
-
-				// Assign the node's tier
-				else {
-					node.tier = node.parent.tier + 1;
-					nodeList.splice(i,1);
-
-					// An array for the specific tier does not exist,
-					// so make the array and add the node.
-					if (sortedNodes[node.tier] == undefined) {
-						sortedNodes[node.tier] = [node];
-					}
-
-					// An array for the specific tier exists so add the node
-					else {
-						sortedNodes[node.tier].push(node);
+				// Check each node that is already sorted,
+				// to see if it's a parent of the node we're checking.
+				for (var x; x < sortedNodes.length; x++) {
+					var array = sortedNodes[x];
+					for (var parent in array) {
+						// We found the parent!
+						if (node.parent.name == parent.name) {
+							// Case 1: we don't have an array for that tier yet.
+							if (sortedNodes[x+1] == undefined) {
+								sortedNodes[x+1] = [node];
+							}
+							// Case 2: we have an array for that tier already.
+							else {
+								sortedNodes[x+1].push(node);
+							}
+							// Remove node from nodeList
+							nodeList.splice(i,1);
+						}
 					}
 				}
 			}

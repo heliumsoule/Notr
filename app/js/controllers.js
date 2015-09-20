8d1c5a6cfd17d8e3aaa9e098ab06d0b8367bb2d3
hieusydo <hieusydo@gmail.com>
2015-09-20 00:21:30 -0400

    Test pdfmaker

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,19 @@
 /* Controllers */
 var notrApp = angular.module('notrApp', ['ui.ace']);
 
-notrApp.controller('RawCtrl', function ($scope) {
+notrApp.controller('RawCtrl', function($scope) {
 	$scope.showBox = function() {
 		alert("Hello world");
 	};
-	
+});
+
+notrApp.directive('exportPdf', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			// console.log(element);
+			var clickZone = element.children();
+			console.log(clickZone);
+		}
+	}
 });
